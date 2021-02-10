5d6034322d594f4e6b5cb60f1f4e396db1832ac9
scordee <deepak.tunuguntla@gmail.com>
2021-02-10 16:38:10 +0100

    Added counts per label function

--- pigeonXT/annotate.py
+++ pigeonXT/annotate.py
@@ -206,3 +206,12 @@ def annotate(examples,
     next_example()
     return annotations
 
+
+def get_number_of_annotations_per_label(annotations, labels):
+    annotations_count = {}
+    chosen_labels_list = [annotated_labels for annotated_labels in annotations.values()]
+
+    for label in labels:
+        annotations_count[label] = sum(label in chosen_labels for chosen_labels in chosen_labels_list)
+
+    return annotations_count
